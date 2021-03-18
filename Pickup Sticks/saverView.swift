01b3f751dd844fee342eea3d7735ffb9ebc56c3f
david reinfurt <reinfurt@o-r-g.com>
2021-03-17 22:35:58 -0400

    add restitution, friction to physics, pick colors for sticks only from r, g, b, y

--- Pickup Sticks/saverView.swift
+++ Pickup Sticks/saverView.swift
@@ -29,6 +29,8 @@ class saverView: ScreenSaverView {
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
         
+scene.physicsWorld.speed = 2.0  // 1.0 default ("real"time)
+
         // place the camera
         // first set rotation (eulerAngle)
         // then adjust position
@@ -36,10 +38,10 @@ class saverView: ScreenSaverView {
 
         // plan view
         cameraNode.eulerAngles = SCNVector3(x: -.pi/2, y: 0, z: 0);
-        cameraNode.position = SCNVector3(x: 0, y: 50, z: 0)
+        cameraNode.position = SCNVector3(x: 0, y: 30, z: 0)
 
         // off-axis front view
-        // cameraNode.position = SCNVector3(x: 0, y: 25, z: 100)
+        // cameraNode.position = SCNVector3(x: 0, y: 10, z: 50)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -57,23 +59,14 @@ class saverView: ScreenSaverView {
         ambientLightNode.light!.color = NSColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
 
-        /*
-        // add sticks
-        // for _ in 0..<50 {
-        for _ in 0..<sticks {
-            let stickNode = createStickNode()
-            scene.rootNode.addChildNode(stickNode)
-        }
-        */
-
         // add floor
         let floor = createFloor()
         scene.rootNode.addChildNode(floor)
 
         // add sticks
-        for i in 0..<50 {
-            let box = createBox(size: CGFloat(5.0))
-            scene.rootNode.addChildNode(box)
+        for i in 0..<40 {
+            let stick = createStick(size: CGFloat(5.0))
+            scene.rootNode.addChildNode(stick)
         }
 
         // need to add pivot to change orientation before rotation ?
@@ -95,46 +88,6 @@ class saverView: ScreenSaverView {
         // scnView?.antialiasingMode = .None
     }
     
-    func createStickNode() -> SCNNode {
-
-        let stick = SCNBox(width: 0.025*scale, height: 0.025*scale, length: 3.0*scale, chamferRadius: 0.1)
-        
-        // color
-        stick.firstMaterial?.diffuse.contents = NSColor(
-                                red: .random(in: 0...1),
-                                green: .random(in: 0...1),
-                                blue: .random(in: 0...1),
-                                alpha: 1.0)
-        let stickNode = SCNNode(geometry: stick)
-        
-        // position
-        let randomDouble = Double.random(in: 1..<5)
-        let stickNodePosition = CGFloat(Double.pi*2/randomDouble)
-        stickNode.position = SCNVector3(x: 0, y: offset, z: 0)
-        // stickNode.rotation = SCNVector4(x: 0, y: 0, z: 1, w: .pi)
-        stickNode.runAction(SCNAction.rotateBy(x: 0, y: -stickNodePosition, z: 0, duration: 6))
-        stickNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: -stickNodePosition, z: 0, duration: 12)))
-        
-        // physics
-        // https://developer.apple.com/documentation/scenekit/physics_simulation
-        // https://developer.apple.com/documentation/scenekit/scnphysicsbody
-        // https://www.raywenderlich.com/1258-scene-kit-tutorial-with-swift-part-3-physics
-        
-        // let location = touch.location(in: self)
-        // let size = CGSize(width: 512, height: 2)
-        // let stick = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
-        // stickNode.physicsBody = SCNPhysicsBody(rectangleOf: stick.size)
-        // stickNode.physicsBody = SCNPhysicsBody()
-        stickNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
-        // stickNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
-
-        // gravity? adjustment
-        // doesnt seem to do anything
-        // stickNode.physicsBody?.restitution = 0.05
-
-        return stickNode
-    }
-
     func createFloor() -> SCNNode {
 
         let floor = SCNBox(width: 100.0, height: 1.0, length: 100.0, chamferRadius: 0.0)        
@@ -151,35 +104,82 @@ class saverView: ScreenSaverView {
         return floorNode
     }
 
-    func createBox(size: CGFloat) -> SCNNode {
+    func createStick(size: CGFloat) -> SCNNode {
 
-        var box:SCNGeometry
+// should make size work in function
+
+        var stick:SCNGeometry
 
         // width and height and length all effect gravity substantially
-        box = SCNBox(width: 0.03 * size, height: 0.03 * size, length: 7.0 * size, chamferRadius: 0.1)
-        box.firstMaterial?.diffuse.contents = NSColor(
+        stick = SCNBox(width: 0.02 * size, height: 0.02 * size, length: 6.0 * size, chamferRadius: 0.15)
+        /*
+        stick.firstMaterial?.diffuse.contents = NSColor(
                                 red: .random(in: 0...1),
                                 green: .random(in: 0...1),
                                 blue: .random(in: 0...1),
                                 alpha: 1.0)
-        let boxNode = SCNNode(geometry: box)
-        boxNode.position = SCNVector3(x: 0, y: 20 * size, z: 0)
-        boxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        */
+        /*
+        let red = NSColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        let green = NSColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+        let blue = NSColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        let yellow = NSColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
+        */
 
+        /*            
+            r 86%, 31%, 26%
+            g 21%, 48%, 25%
+            b 16%, 47%, 70%
+            y 93%, 87%, 34%
+        */
+        let red = NSColor(red: 0.86, green: 0.31, blue: 0.26, alpha: 1.0)
+        let green = NSColor(red: 0.21, green: 0.48, blue: 0.25, alpha: 1.0)
+        let blue = NSColor(red: 0.16, green: 0.47, blue: 0.70, alpha: 1.0)
+        let yellow = NSColor(red: 0.93, green: 0.87, blue: 0.34, alpha: 1.0)
+
+        let colorIndex = Int.random(in: 0...3)
+
+        switch colorIndex {
+        case 0:
+            stick.firstMaterial?.diffuse.contents = red
+        case 1:
+            stick.firstMaterial?.diffuse.contents = green
+        case 2:
+            stick.firstMaterial?.diffuse.contents = blue
+        case 3:
+            stick.firstMaterial?.diffuse.contents = yellow
+        default:
+            stick.firstMaterial?.diffuse.contents = yellow
+            // print("Some other character")
+        }
+
+        let stickNode = SCNNode(geometry: stick)
+        stickNode.position = SCNVector3(x: 0, y: 5 * size, z: 0)
+        stickNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        // nil in above sets the bounding box to match the given geometry
+
+        // https://developer.apple.com/documentation/scenekit/scnphysicsbody/
+        // more physics body settings
+                
+        stickNode.physicsBody?.restitution = 0.85   // ? try unwrap
+        stickNode.physicsBody?.friction = 0.5   // 0.5 default
+
+        // stickNode.physicsBody?.mass = 0.5   // 1.0 is default
+        // stickNode.physicsBody!.restitution = 0.75   // ! force unwrap
 
         // position
         let randomDouble = Double.random(in: 1..<5)
         let rotation = CGFloat(Double.pi*2/randomDouble)
         // stickNode.position = SCNVector3(x: 0, y: offset, z: 0)
         // stickNode.rotation = SCNVector4(x: 0, y: 0, z: 1, w: .pi)
-        // boxNode.runAction(SCNAction.rotateBy(x: 0, y: -stickNodePosition, z: 0, duration: 6))
-        // boxNode.runAction(SCNAction.rotateBy(x: 0, y: .random(in: 0...3.141519), z: 0, duration: 0.5))
-        // boxNode.runAction(SCNAction.rotateBy(x: 0, y: .random(in: 0...1), z: 0, duration: 0.5))
-        boxNode.runAction(SCNAction.rotateBy(x: .random(in: 0...1), y: .random(in: 0...1), z: 0, duration: 0.25))
-        // boxNode.runAction(SCNAction.rotateBy(x: 0, y: rotation, z: 0, duration: 0.25))
+        // stickNode.runAction(SCNAction.rotateBy(x: .random(in: 0...1), y: .random(in: 0...1), z: 0, duration: 0.25))
+        // stickNode.runAction(SCNAction.rotateBy(x: .random(in: 0...0.5), y: .random(in: 0...1), z: 0, duration: 0.25))
+        // stickNode.runAction(SCNAction.rotateBy(x: .random(in: 0...0.5), y: .random(in: 0...0.5), z: 0, duration: 0.25))
+        stickNode.runAction(SCNAction.rotateBy(x: 0, y: .random(in: -0.2...0.2), z: 0, duration: 0.05))
         // stickNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: -stickNodePosition, z: 0, duration: 12)))
+        // stickNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: .random(in: 0...1), z: 0, duration: 0.25)))
 
-        return boxNode
+        return stickNode
     }
     
     override init?(frame: NSRect, isPreview: Bool) {
